9538cdc171f132b44383f6c4fddacca8601a5e2f
Xu-Andrew <andrew_xu2006@yahoo.com>
2016-09-17 21:01:18 -0400

    added main.py

---
+++ pnfcharts/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, send_from_directory
+
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_url_path='/')
+
+@app.route('/<path:path>')
+def send_js(path):
+    return send_from_directory('.',path)
+
+
+@app.route('/')
+def send_index():
+    return send_from_directory('.','index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
