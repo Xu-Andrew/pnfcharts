af86264fca4ca0717728f5cfc38133d7e317c0e8
Xu-Andrew <andrew_xu2006@yahoo.com>
2016-09-18 00:08:03 -0400

    main.py

--- pnfcharts/main.py
+++ pnfcharts/main.py
@@ -5,13 +5,13 @@ app = Flask(__name__, static_url_path='/')
 
 @app.route('/<path:path>')
 def send_js(path):
-    return send_from_directory('.',path)
+	return send_from_directory('.',path)
 
 
 @app.route('/')
 def send_index():
-    return send_from_directory('.','index.html')
+	return send_from_directory('.','index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(host="0.0.0.0", debug=True)
